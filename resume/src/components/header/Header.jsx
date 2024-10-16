b0b749e39e485cb9907002b80c3c42d47ab5934c
vikdts <v.d.tsareva@gmail.com>
2024-10-16 15:13:08 +0300

    add logo link

--- resume/src/components/header/Header.jsx
+++ resume/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
         <nav className="nav container">
-            
+            <a href="index.html" className="nav__logo">asd</a>
         </nav>
     </header>
   )
