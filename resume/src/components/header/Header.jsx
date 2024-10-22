f5263969e20694372effb32c7a1728477f7c1c0f
vikdts <v.d.tsareva@gmail.com>
2024-10-22 13:26:01 +0300

    add list items

--- resume/src/components/header/Header.jsx
+++ resume/src/components/header/Header.jsx
@@ -8,7 +8,31 @@ const Header = () => {
             <a href="index.html" className="nav__logo">asd</a>
             <div className="nav__menu">
                 <ul className="nav__list grid">
-                    
+                    <li className="nav__item">
+                        <a href="" className="nav__link">
+                            <i className="nav__icon">Home</i>
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="" className="nav__link">
+                            <i className="nav__icon">About</i>
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="" className="nav__link">
+                            <i className="nav__icon">Skills</i>
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="" className="nav__link">
+                            <i className="nav__icon">Work</i>
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="" className="nav__link">
+                            <i className="nav__icon">Find me</i>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
