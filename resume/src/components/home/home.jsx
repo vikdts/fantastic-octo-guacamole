1809de77c709e9681c521c0de212291507213223
vikdts <v.d.tsareva@gmail.com>
2025-01-18 19:12:23 +0200

    import home.css and social

--- resume/src/components/home/home.jsx
+++ resume/src/components/home/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import "./home.css";
+import Social from '../../social';
 
 const Home = () => {
   return (
     <section className="home" id="home">
         <div className="home__container container grid">
             <div className="home__content grid">
-                
+
             </div>
         </div>
     </section>
