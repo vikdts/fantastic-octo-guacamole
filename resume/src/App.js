31201d4101f9d005fb592a53d484da3e17aadfd1
vikdts <v.d.tsareva@gmail.com>
2025-01-18 17:47:31 +0200

    initialize state show menu

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <>
+     <div className="App">
       <Header/>
-    </div>
+     </div>
+    </>
   );
 }
 
--- resume/src/components/header/Header.jsx
+++ resume/src/components/header/Header.jsx
@@ -1,43 +1,60 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+  const [Toggle, showMenu] = useState(false);
   return (
     <header className="header">
-        <nav className="nav container">
-            <a href="index.html" className="nav__logo">VIK</a>
-            <div className="nav__menu">
-                <ul className="nav__list grid">
-                    <li className="nav__item">
-                        <a href="home" className="nav__link active-link">HOME</a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="about" className="nav__link active-link">ABOUT</a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="skills" className="nav__link active-link">SKILLS</a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="services" className="nav__link active-link">SERVICES</a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="work" className="nav__link active-link">WORK</a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="findme" className="nav__link active-link">FIND ME</a>
-                    </li>
-                </ul>
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          VIK
+        </a>
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                HOME
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                ABOUT
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                SKILLS
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                SERVICES
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#work" className="nav__link">
+                WORK
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#findme" className="nav__link">
+                FIND ME
+              </a>
+            </li>
+          </ul>
 
-                <i className="uil uil-times nav__close"></i>
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
+        </div>
 
-                <div className="nav__toggle">
-                    <i className="uil uil-apps"></i>
-                </div>
-                
-            </div>
-        </nav>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
