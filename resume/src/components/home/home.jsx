b7024138f859ca12ab48ec0cc273a2fbc1907c9e
vikdts <v.d.tsareva@gmail.com>
2025-01-19 09:22:48 +0200

    update data comp

--- resume/src/components/home/home.jsx
+++ resume/src/components/home/home.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import "./home.css";
-import Social from '../../social';
+import Social from './social';
 import Data from './data';
 
 const Home = () => {
---
+++ resume/src/components/home/social.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/vikdts"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/viktoriya-tsareva-b963b3118/?originalSubdomain=bg"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
