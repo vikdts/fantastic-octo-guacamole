35d63932f1f0f886731ba188340a07423f1fefce
vikdts <v.d.tsareva@gmail.com>
2025-01-19 11:01:49 +0200

    update subtitle

--- resume/src/components/home/data.jsx
+++ resume/src/components/home/data.jsx
@@ -3,14 +3,11 @@ import React from "react";
 const Data = () => {
   return (
     <div className="home__data">
-      <h1 className="home__title">
-      Vik Tsareva
-      </h1>
-      <h3 className="home__subtitle">Visual Designer</h3>
+      <h1 className="home__title">Vik Tsareva</h1>
+      <h3 className="home__subtitle">JS Enthusiast</h3>
       <p className="home__description">
-        Lorem ipsum dolor sit, 
-                amet consectetur adipisicing elit. Non necessitatibus 
-                sapiente magni eius doloribus odio unde optio.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non
+        necessitatibus sapiente magni eius doloribus odio unde optio.
       </p>
 
       <a href="#contact" className="button button--flex">
